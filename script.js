dd3deaf987b51fb9d67cc4adbde89369f5286389
NoorAlkari <nour.alkadri1234@gmail.com>
2025-11-16 17:01:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -48,11 +48,12 @@ function renderItems(list) {
     return;
   }
 
-  itemsBody.innerHTML = list.map(item => `
+  itemsBody.innerHTML = list.
+    map(item) => `
     <tr>
       <td>
         <div class="item-cell">
-          <img src="images/${item.image}" class="item-icon" alt="${item.name}">
+          <img src="../images/${item.image}" class="item-icon" alt="${item.name}">
           <span class="item-name">${item.name}</span>
         </div>
       </td>
@@ -286,6 +287,7 @@ filterAndRender();
 renderBackpack();
 renderInventory();
 renderWallet();
+
 
 
 
